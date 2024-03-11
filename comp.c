b0d9f73de3b4f293d4dee58e07a535a020aaed59
Quan Pham <mquan.start@icloud.com>
2024-03-10 23:54:54 -0400

    format

--- comp.c
+++ comp.c
@@ -17,19 +17,19 @@ int main (int argc, char **argv) {
   if (retval == 0) {
     visit_ast(fill_table);
     retval = visit_ast(declare_var_before_use);
-    if (retval) printf("Error: Variable not declared\n");
+    if (retval) printf("");
     retval = visit_ast(declare_func_before_use);
-    if (retval) printf("Error: Function not defined\n");
+    if (retval) printf("");
     retval = visit_ast(match_num_args_func);
-    if (retval) printf("Error: Number of arguments in function call does not match definition\n");
+    if (retval) printf("");
     retval = visit_ast(unique_func_names);
-    if (retval) printf("Error: Function name defined twice\n");
+    if (retval) printf("");
     retval = visit_ast(vars_with_func_names);
-    if (retval) printf("Error: Variable shares a name of a defined function\n");
+    if (retval) printf("");
     retval = visit_ast(duplicate_var_declare);
-    if (retval) printf("Error: Variable declared twice\n");
+    if (retval) printf("");
     retval = visit_ast(duplicate_arg_func);
-    if (retval) printf("Error: Function argument defined twice\n");
+    if (retval) printf("");
     st_print();  // should not print if any failures occured
     print_ast(); 
   }
