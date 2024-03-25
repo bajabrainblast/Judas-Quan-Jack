310d6307d0026830fa7384854e17c49f8c661a1f
jdewey2023 <jcd18c@fsu.edu>
2024-03-24 20:02:48 -0400

    End program after error; remove misc. commented code; added cleanup function to comp

--- comp.c
+++ comp.c
@@ -7,50 +7,42 @@
 #include "map.h"
 int yyparse();
 
-int main (int argc, char **argv) {
-  extern struct stack st;
-  st.top = NULL;
-
-  extern struct sym_table table;
-  table.start = NULL; 
-
-  extern struct type_map map;
-  map.start = NULL;
-
-  int retval = yyparse();
-  if (retval == 0) {
-    visit_ast(fill_table);
-    if (visit_ast(declare_var_before_use)) return 1;
-    if (visit_ast(declare_func_before_use)) return 2;
-    if (visit_ast(match_num_args_func)) return 3;
-    if (visit_ast(unique_func_names)) return 4;
-    if (visit_ast(vars_with_func_names)) return 5;
-    if (visit_ast(duplicate_var_declare)) return 6;
-    if (visit_ast(duplicate_arg_func)) return 7;
-    visit_ast(init_map);
-    //tm_print();
-    // pass over as many times as needed until zero unknowns remain
-    
-    while (visit_ast(fill_map) == 0) {
-      printf("passed\n");
-    }
-    
-    //visit_ast(fill_map);
-    tm_print();
-
-    st_print();  // should not print if any failures occured
-    print_ast(); 
-
-    visit_ast(check_function_returns);
-    visit_ast(well_formed_aop);
-    visit_ast(well_formed_bop);
-    visit_ast(if_first_arg);
-    visit_ast(func_call_args_type);
-    visit_ast(check_ifs);
-    visit_ast(check_lets);
-  }
+int cleanup(int error){
   free_ast();
   st_free();
   tm_free();
-  return retval;
+  return error;
+}
+
+int main (int argc, char **argv) {
+  extern struct stack st;
+  st.top = NULL;
+  extern struct sym_table table;
+  table.start = NULL; 
+  extern struct type_map map;
+  map.start = NULL;
+  if (!yyparse()) {
+    visit_ast(fill_table);
+    if (visit_ast(declare_var_before_use)) return cleanup(1);
+    if (visit_ast(declare_func_before_use)) return cleanup(2);
+    if (visit_ast(match_num_args_func)) return cleanup(3);
+    if (visit_ast(unique_func_names)) return cleanup(4);
+    if (visit_ast(vars_with_func_names)) return cleanup(5);
+    if (visit_ast(duplicate_var_declare)) return cleanup(6);
+    if (visit_ast(duplicate_arg_func)) return cleanup(7);
+    visit_ast(init_map);
+    while (visit_ast(fill_map) == 0) {
+      printf("passed\n");
+    }
+    print_ast();
+    if (visit_ast(check_function_returns)) return cleanup(8);
+    if (visit_ast(well_formed_aop)) return cleanup(9);
+    if (visit_ast(check_function_returns)) return cleanup(10);
+    if (visit_ast(well_formed_bop)) return cleanup(11);
+    if (visit_ast(if_first_arg)) return cleanup(12);
+    if (visit_ast(func_call_args_type)) return cleanup(13);
+    if (visit_ast(check_ifs)) return cleanup(14);
+    if (visit_ast(check_lets)) return cleanup(15);
+  }
+  return cleanup(0);
 }
